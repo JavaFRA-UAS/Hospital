257a677ba3f285b6f6c713e76f71c0f6be4bcbf9
Tobias Schulz <tobiasschulz.dev@outlook.com>
2017-01-13 06:54:59 +0100

    ...

--- Hospital.bat
+++ Hospital.bat
@@ -1,1 +1,1 @@
-java -cp "lib/jgoodies-common-1.9.0.jar;lib/jgoodies-forms-1.9.0.jar;lib/sqlite-jdbc-3.15.1.jar;lib/LGoodDatePicker-8.3.0.jar;bin" hospital.Main
+java -cp "lib/jgoodies-common-1.9.0.jar;lib/jgoodies-forms-1.9.0.jar;lib/sqlite-jdbc-3.15.1.jar;lib/LGoodDatePicker-8.3.0.jar;bin;lib/hospital.jar" hospital.Main
